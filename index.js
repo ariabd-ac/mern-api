949b7bbffb9eea752cb643565a3d5909ef88bd2d
ariabd-ac <ariabd.com@gmail.com>
2020-11-22 22:10:51 +0700

    mern api

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
+
+const app = express();
+
+const authRoute = require("./src/routes/auth");
+const blogRoute = require("./src/routes/blog");
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(
+      null,
+      new Date().getTime() + "-" + file.originalname
+      // new Date().toString().replace(/:/g, "-") + "-" + file.originalname
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+app.use(bodyParser.json()); //type json
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader(
+//     "Access-Control-Allow-Methods",
+//     "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+//   );
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+//   next();
+// });
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, X-Requested-With, Content-Type, Authorization, Accept"
+  );
+  next();
+});
+
+app.use("/v1/auth", authRoute);
+app.use("/v1/blog", blogRoute);
+
+app.use((error, req, res, next) => {
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://ariabd:e7rmOqeH7xS5ALri@cluster0.rbw2g.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(4000, () => console.log("connected"));
+  })
+  .catch((err) => console.log(err));
---
+++ src/routes/blog.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const { body } = require("express-validator");
+
+const router = express.Router();
+
+const blogController = require("../controllers/blog");
+
+// [POST] : /v1/blog/post
+router.post(
+  "/post",
+  [
+    body("title").isLength({ min: 5 }).withMessage("input min 5"),
+    body("body").isLength({ min: 5 }).withMessage("input min 5"),
+  ],
+  blogController.createBlogPost
+);
+
+router.get("/posts", blogController.getAllBlogPost);
+
+router.get("/post/:postId", blogController.getBlogPostById);
+
+router.put(
+  "/post/:postId",
+  [
+    body("title").isLength({ min: 5 }).withMessage("input min 5"),
+    body("body").isLength({ min: 5 }).withMessage("input min 5"),
+  ],
+  blogController.updateBlogPost
+);
+
+router.delete("/post/:postId", blogController.deleteBlogPost);
+
+module.exports = router;
